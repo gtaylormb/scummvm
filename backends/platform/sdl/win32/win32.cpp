74d635208f86a5033fe2cd26f449f3965f8a4829
Walter van Niftrik <walter@vanniftrik-it.nl>
2015-02-17 21:04:06 +0100

    CONFIGURE: Set _WIN32_WINNT on mingw
    Taskbar code uses Windows 7 API

--- backends/platform/sdl/win32/win32.cpp
+++ backends/platform/sdl/win32/win32.cpp
@@ -28,6 +28,7 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #undef ARRAYSIZE // winnt.h defines ARRAYSIZE, but we want our own one...
+#include <basetyps.h>
 #include <shellapi.h>
 
 #include "common/scummsys.h"
